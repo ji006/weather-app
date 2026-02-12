df7e9783c0b5c12c0d133974babd46a6929f53e4
ji006 <mjkim.peach@gmail.com>
2026-02-13 00:08:01 +0900

    add searchList

--- src/features/search-location/ui/SearchInput.tsx
+++ src/features/search-location/ui/SearchInput.tsx
@@ -1,6 +1,21 @@
 import { Search } from "lucide-react";
+import { useState } from "react";
 
 export const SearchInput = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [suggestions] = useState<any[]>([
+    { id: "1", displayName: "서울특별시", address: "서울특별시" },
+    {
+      id: "2",
+      displayName: "종로구 청운동",
+      address: "서울특별시 종로구 청운동",
+    },
+    {
+      id: "3",
+      displayName: "강남구 역삼동",
+      address: "서울특별시 강남구 역삼동",
+    },
+  ]);
   return (
     <div className="flex w-full justify-center px-4">
       <div className="relative w-full max-w-[912px]">
@@ -12,8 +27,27 @@ export const SearchInput = () => {
         <input
           type="text"
           placeholder="지역을 입력하세요"
-          className="h-[50px] w-full rounded-full border border-purple bg-white bg-opacity-30 pl-12 pr-4 text-white placeholder:text-gray-500"
+          className={`h-[50px] w-full border border-purple bg-white pl-12 pr-4 text-gray-800 shadow-sm placeholder:text-gray-500 focus:outline-none ${
+            isOpen && suggestions.length > 0
+              ? "rounded-t-[25px] border-b-transparent" // 리스트가 열리면 아래쪽 둥근 모서리 제거 및 선 없앰
+              : "rounded-full bg-opacity-40" // 평소에는 완전 둥글게
+          }`}
         />
+        {isOpen && (
+          <ul className="absolute z-10 w-full overflow-hidden rounded-b-3xl border border-purple border-t-gray-300 bg-white/95 shadow-2xl">
+            {suggestions.map((region) => (
+              <li
+                key={region.id}
+                className="flex cursor-pointer flex-col border-t-2 border-gray-100 bg-opacity-35 px-12 py-3 first:border-none hover:bg-[#E1E8F2]"
+              >
+                <span className="text-sm font-bold text-gray-800 md:text-base">
+                  {region.displayName}
+                </span>
+                <span className="text-xs text-gray-500">{region.address}</span>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
