238bec3aa88110825e23820d082748a67a57d503
ji006 <mjkim.peach@gmail.com>
2026-02-12 15:04:22 +0900

    add header

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,15 @@
 // src/pages/home/ui/HomePage.tsx
-import { SearchInput } from '../../../features/search-location/ui/SearchInput';
+import { SearchInput } from "../../../features/search-location/ui/SearchInput";
+import { Header } from "../../../widgets/header/ui/Header";
 
 export const HomePage = () => {
   return (
     // 배경 그라데이션 (이미지 느낌)
     <div className="min-h-screen w-full bg-gradient-to-b from-[#A7D5E4] to-[#80A9E9]">
+      <Header />
+      <section className="w-full mt-2">
         <SearchInput />
+      </section>
     </div>
   );
 };
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,18 @@
+import { Star, CloudMoon } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <div className="relative mx-auto flex w-full max-w-[912px] items-center justify-between px-6 py-8">
+      <div className="" />
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer">
+        <CloudMoon className="h-[55px] w-[55px] text-white" />
+      </div>
+      <div className="flex flex-col items-end">
+        <div className="flex cursor-pointer flex-col items-center">
+          <Star className="h-8 w-8 fill-slate-300 text-slate-400" />
+          <span className="mt-1 text-[9px] font-bold text-black">즐겨찾기</span>
+        </div>
+      </div>
+    </div>
+  );
+};
